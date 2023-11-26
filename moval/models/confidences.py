fixing bugs, we should get all pixel for bg, not class averaged one

moval/models/confidences.py
@@ -77,12 +77,12 @@ class Confidence(abc.ABC):
             conf_score: The calculated confidence scores, of shape ``(n, )`` or a list of n ``(H, W, (D))``.
         
         """
+        if T is None:
+            T = np.array([1])
 
         if isinstance(inp, list):
             # inp is a list, segmentaiton task
             num_class = inp[0].shape[0]
-            if T is None:
-                T = np.ones(num_class)
 
             conf_scores = []
 
@@ -108,9 +108,9 @@ class Confidence(abc.ABC):
                     conf_score = 1 - conf_score
 
                 # transfer back to the original dimension
-                conf_score = conf_score.reshape(inp_shape[:1] + inp_shape[2:]) # ``(1, H, W, (D))``
+                conf_score = conf_score.reshape(inp_shape[2:]) # ``(H, W, (D))``
             
-                conf_scores.append(conf_score[0])
+                conf_scores.append(conf_score)
 
             return conf_scores
 
@@ -118,8 +118,6 @@ class Confidence(abc.ABC):
             # inp is not a list, classification task
 
             num_class = inp.shape[1]
-            if T is None:
-                T = np.ones(num_class)
 
             pred = np.argmax(inp, axis=1)
             conf_score = np.zeros(inp.shape[0]) # ``(n, )``

moval/solvers/criterions.py
@@ -92,33 +92,51 @@ class segCalibrate(Calibrate):
         """
 
         if not self.class_specific:
-            
-            err_all = []
+            pred_all_flatten = []
+            gt_all_flatten = []
             for n_case in range(len(inp)):
 
                 pred_case   = np.argmax(inp[n_case], axis = 0) # ``(H, W, (D))``
                 gt_case     = gt[n_case] # ``(H, W, (D))``
-                acc = np.sum(gt_case.flatten() == pred_case.flatten()) / len(gt_case.flatten())
-                err = estim - acc
-                err_all.append(err)
+                
+                pred_all_flatten.append(pred_case.flatten())
+                gt_all_flatten.append(gt_case.flatten())
+            
+            pred_all_flatten = np.concatenate(pred_all_flatten)
+            gt_all_flatten = np.concatenate(gt_all_flatten)
+
+            acc = np.sum(gt_all_flatten == pred_all_flatten) / len(gt_all_flatten)
+            err = estim - acc
+
+            return np.abs(err)
 
         else:
             
-            err_all = []
+            pred_all_flatten_bg = []
+            gt_all_flatten_bg = []
+            dsc = []
+
             for n_case in range(len(inp)):
 
                 pred_case   = np.argmax(inp[n_case], axis = 0) # ``(H, W, (D))``
                 gt_case     = gt[n_case] # ``(H, W, (D))``
-                err = np.zeros(inp[n_case].shape[0])
                 
                 pos_bg = np.where(pred_case.flatten() == 0)[0]
-                acc_bg = np.sum(gt_case.flatten()[pos_bg] == pred_case.flatten()[pos_bg]) / len(gt_case.flatten()[pos_bg])
-                err[0] = estim[0] - acc_bg
 
+                pred_all_flatten_bg.append(pred_case.flatten()[pos_bg])
+                gt_all_flatten_bg.append(gt_case.flatten()[pos_bg])
+
+                dsc_case = np.zeros(inp[n_case].shape[0])
                 for kcls in range(1, inp[n_case].shape[0]):
-                    err[kcls] = estim[kcls] - ComputMetric(pred_case == kcls, gt_case == kcls)
-                err_all.append(err)
+                    dsc_case[kcls] = ComputMetric(pred_case == kcls, gt_case == kcls)
+                dsc.append(dsc_case)
 
-        m_err = np.mean(np.array(err_all), axis=0)
+            pred_all_flatten_bg = np.concatenate(pred_all_flatten_bg)
+            gt_all_flatten_bg = np.concatenate(gt_all_flatten_bg)
 
-        return np.abs(m_err)
+            acc_bg = np.sum(gt_all_flatten_bg == pred_all_flatten_bg) / len(gt_all_flatten_bg)
+            
+            err = estim - np.mean(np.array(dsc), axis=0)
+            err[0] = estim[0] - acc_bg
+
+            return np.abs(err)

moval/solvers/solver.py
@@ -92,7 +92,7 @@ class Solver(abc.ABC):
         self,
         inp: Union[List[Iterable], np.ndarray],
         gt: Union[List[Iterable], np.ndarray]
-    ):
+    ) -> Union[Tuple[np.ndarray, np.ndarray], np.ndarray]:
         """Fit the model based on scipy backend.
         
         Args:
@@ -104,6 +104,14 @@ class Solver(abc.ABC):
             If the model contains extended parameters, will return Tuple[param, param_ext].
 
         """
+        if self.model.estim_algorithm == "ac-model":
+            # do not need optimization.
+            
+            self.model.eval()
+            self.model.is_fitted = True
+
+            return self.model.param
+
         self.inp = inp
         self.gt = gt
 
@@ -152,7 +160,7 @@ class Solver(abc.ABC):
         
             optimized_param = optimization_result.x
 
-            if (optimization_result.fun > search_threshold) and (self.model.estim_algorithm != "ac-model"):
+            if optimization_result.fun > search_threshold:
                 # change the initial state, if we are not satisfied with the optimization results.
                 results = []
                 results.append((optimization_result.fun, optimization_result.x))
@@ -183,7 +191,7 @@ class Solver(abc.ABC):
                     
                     optimized_param = optimization_result.x[0]
 
-                    if (optimization_result.fun > search_threshold) and (self.model.estim_algorithm != "ac-model"):
+                    if optimization_result.fun > search_threshold:
                         # change the initial state, if we are not satisfied with the optimization results.
                         results = []
                         results.append((optimization_result.fun, optimization_result.x[0]))

tests/test_demo_cls.py
@@ -55,14 +55,13 @@ if os.path.isfile(results_files):
     os.remove(results_files)
 
 @pytest.mark.parametrize(
-        "estim_algorithm, mode, numclass, confidence_scores, class_specific", 
+        "estim_algorithm, mode, confidence_scores, class_specific", 
         list(itertools.product(moval.models.get_estim_options(),
                                ["classification"],
-                               [10], 
                                moval.models.get_conf_options(),
                                [False, True])),
 )
-def test_cls(estim_algorithm, mode, numclass, confidence_scores, class_specific):
+def test_cls(estim_algorithm, mode, confidence_scores, class_specific):
 
     moval_model = moval.MOVAL(
                 mode = mode,
@@ -83,7 +82,7 @@ def test_cls(estim_algorithm, mode, numclass, confidence_scores, class_specific)
     pred_test = np.argmax(logits_test, axis = 1)
     err_test = np.abs( np.sum(gt_test == pred_test) / len(gt_test) - estim_acc_test )
 
-    test_condition = f"estim_algorithm = {estim_algorithm}, mode = {mode}, numclass = {numclass}, confidence_scores = {confidence_scores}, class_specific = {class_specific}"
+    test_condition = f"estim_algorithm = {estim_algorithm}, mode = {mode}, confidence_scores = {confidence_scores}, class_specific = {class_specific}"
 
     with open(results_files, 'a') as f:
         f.write(test_condition)

tests/test_demo_seg_3d.py
@@ -40,24 +40,18 @@ logits = []
 gt = []
 for Imgname_eval in Imglist_eval_read:
     GT_file = Imgname_eval.replace("data", "data_moval")
-
     caseID = Imgname_eval.split("/")[-1][:6]
-
     logit_cls0_file = "data_moval/Prostateresults/prostateval/results/pred_" + caseID + "cls0_prob.nii.gz"
-    logit_cls1_file = "data_moval/Prostateresults/prostateval/results/pred_" + caseID + "cls0_prob.nii.gz"
-
+    logit_cls1_file = "data_moval/Prostateresults/prostateval/results/pred_" + caseID + "cls1_prob.nii.gz"
     logit_cls0_read = nib.load(logit_cls0_file)
     logit_cls1_read = nib.load(logit_cls1_file)
     logit_cls0      = logit_cls0_read.get_fdata()   # ``(H, W, D)``
     logit_cls1      = logit_cls1_read.get_fdata()
     GT_read         = nib.load(GT_file)
     GTimg           = GT_read.get_fdata()           # ``(H, W, D)``
-
     logit_cls = np.stack((logit_cls0, logit_cls1))  # ``(d, H, W, D)``
-    
     logits.append(logit_cls)
     gt.append(GTimg)
-
 
 # logits is a list of length ``n``,  each element has ``(d, H, W, D)``. 
 # gt is a list of length ``n``,  each element has ``(H, W, D)``.
@@ -75,7 +69,7 @@ for Imgname_test in Imglist_test_read:
     caseID = Imgname_test.split("/")[-1][:6]
 
     logit_cls0_file = "data_moval/Prostateresults/prostattestcondition_A/results/pred_" + caseID + "cls0_prob.nii.gz"
-    logit_cls1_file = "data_moval/Prostateresults/prostattestcondition_A/results/pred_" + caseID + "cls0_prob.nii.gz"
+    logit_cls1_file = "data_moval/Prostateresults/prostattestcondition_A/results/pred_" + caseID + "cls1_prob.nii.gz"
 
     logit_cls0_read = nib.load(logit_cls0_file)
     logit_cls1_read = nib.load(logit_cls1_file)
@@ -99,15 +93,20 @@ results_files = "results_demo_seg_3d.txt"
 if os.path.isfile(results_files):
     os.remove(results_files)
 
+
+# estim_algorithm = "ac-model"
+# mode = "segmentation"
+# confidence_scores = "max_class_probability-conf"
+# class_specific = False
+
 @pytest.mark.parametrize(
-        "estim_algorithm, mode, numclass, confidence_scores, class_specific", 
+        "estim_algorithm, mode, confidence_scores, class_specific", 
         list(itertools.product(moval.models.get_estim_options(),
                                ["segmentation"],
-                               [2], 
                                moval.models.get_conf_options(),
                                [False, True])),
 )
-def test_seg_3d(estim_algorithm, mode, numclass, confidence_scores, class_specific):
+def test_seg_3d(estim_algorithm, mode, confidence_scores, class_specific):
 
     moval_model = moval.MOVAL(
                 mode = mode,
@@ -121,7 +120,7 @@ def test_seg_3d(estim_algorithm, mode, numclass, confidence_scores, class_specif
     estim_dsc = moval_model.estimate(logits)
     
     DSC_list = []
-    for n_case in len(logits):
+    for n_case in range(len(logits)):
         pred_case   = np.argmax(logits[n_case], axis = 0) # ``(H, W, (D))``
         gt_case     = gt[n_case] # ``(H, W, (D))``
 
@@ -129,14 +128,14 @@ def test_seg_3d(estim_algorithm, mode, numclass, confidence_scores, class_specif
         DSC_list.append(DSC)
     m_DSC = np.mean(np.array(DSC_list))
     
-    err_val = np.abs( m_DSC - estim_dsc )
+    err_val_dsc = np.abs( m_DSC - estim_dsc )
 
     # save the test err in the result files.
 
     estim_dsc_test = moval_model.estimate(logits_test)
 
     DSC_list_test = []
-    for n_case in len(logits_test):
+    for n_case in range(len(logits_test)):
         pred_case   = np.argmax(logits_test[n_case], axis = 0) # ``(H, W, (D))``
         gt_case     = gt[n_case] # ``(H, W, (D))``
 
@@ -146,13 +145,16 @@ def test_seg_3d(estim_algorithm, mode, numclass, confidence_scores, class_specif
 
     err_test = np.abs( m_DSC_test - estim_dsc_test )
 
-    test_condition = f"estim_algorithm = {estim_algorithm}, mode = {mode}, numclass = {numclass}, confidence_scores = {confidence_scores}, class_specific = {class_specific}"
+    test_condition = f"estim_algorithm = {estim_algorithm}, mode = {mode}, confidence_scores = {confidence_scores}, class_specific = {class_specific}"
 
     with open(results_files, 'a') as f:
         f.write(test_condition)
         f.write('\n')
-        f.write("validation err: ")
-        f.write(str(err_val))
+        f.write("validation acc err: ")
+        f.write(str(err_val_acc))
+        f.write('\n')
+        f.write("validation dsc err: ")
+        f.write(str(err_val_dsc))
         f.write('\n')
         f.write("moval parameter: ")
         f.write(str(moval_model.model_.param))

tests/test_solver.py
@@ -21,8 +21,7 @@ def test_registry():
 @pytest.mark.parametrize(
         "estim_algorithm, mode, numclass, confidence_scores, class_specific", 
         list(itertools.product(moval.models.get_estim_options(),
-                            #    ["classification", "segmentation"],
-                               ["segmentation"],
+                               ["classification", "segmentation"],
                                [3], 
                                moval.models.get_conf_options(),
                                [False, True])),
@@ -66,8 +65,8 @@ def test_solver(estim_algorithm, mode, numclass, confidence_scores, class_specif
         inp = []
         gt = []
         for _ in range(5):
-            inp.append(np.random.randn(numclass, 50, 50, 50)) # 5 samples, 3 classes, of volume shape ``(50, 50, 50)``
-            gt.append(np.random.randint(0, numclass, (50, 50, 50)))
+            inp.append(np.random.randn(numclass, 10, 10, 10)) # 5 samples, 3 classes, of volume shape ``(50, 50, 50)``
+            gt.append(np.random.randint(0, numclass, (10, 10, 10)))
 
         _estim_acc, _estim_dsc = model(inp)
         #