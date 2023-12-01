b3dd8dc501c43364c16febd4ebfda463d9406e41
Zeju Li <zejuli@Zejus-MacBook-Pro.local>
2023-12-01 16:13:45 +0000

    I really want to eliminate the warning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,11 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+rst_prolog = r"""
+
+.. |Default:| raw:: html
+
+    <div class="default-value-section"> <span class="default-value-label">Default:</span>
+
+"""
